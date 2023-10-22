f3a87c6b41b6a7cd3d2ba3cbdbce7f1634bb66a5
Tyuiu.DragomeretskiyED@outlook.com <mrtvinksi@mail.ru>
2023-10-22 21:27:53 +0500

    Task3

---
+++ Tyuiu.DragomeretskiyED.Sprint3.Task3.V21.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DragomeretskiyED.Sprint3.Task3.V21.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DragomeretskiyED.Sprint3.Task3.V21.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4096ff2e-751c-41bd-8c5b-1fab4e7b9fa2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.DragomeretskiyED.Sprint3.Task3.V21/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.DragomeretskiyED.Sprint3.Task3.V21.Lib;
+
+namespace Tyuiu.DragomeretskiyED.Sprint3.Task3.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Драгомерецкий Е.Д. | СМАРТб-23-1";
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                            *");
+            Console.WriteLine("* Тема:  алгоритмы циклической структуры                                               *");
+            Console.WriteLine("* Задание #3                                                                           *");
+            Console.WriteLine("* Вариант #21                                                                          *");
+            Console.WriteLine("* Выполнил: Драгомерецкий Егор Дмитриевич | СМАРТб-23-1                                *");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                             *");
+            Console.WriteLine("* Используя цикл foreach заменить цифры на букву e в строке: f3g5ht g4j 34kg4          *");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                     *");
+            Console.WriteLine("****************************************************************************************");
+
+
+            Console.WriteLine("f3g5ht g4j 34kg4");
+            string str = "f3g5ht g4j 34kg4";
+            char c = 'e';
+
+
+
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                                            *");
+            Console.WriteLine("****************************************************************************************");
+
+            Console.WriteLine(ds.ReplaceNumOnChar(str, c));
+
+            Console.ReadLine();
+        }
+    }
+}
