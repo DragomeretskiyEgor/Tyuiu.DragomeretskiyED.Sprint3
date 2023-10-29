cca889211287d39983b42113cf9293e978d9a44b
Tyuiu.DragomeretskiyED@outlook.com <mrtvinksi@mail.ru>
2023-10-29 16:22:25 +0500

    Task5

---
+++ Tyuiu.DragomeretskiyED.Sprint3.Task5.V15.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DragomeretskiyED.Sprint3.Task5.V15.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DragomeretskiyED.Sprint3.Task5.V15.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4d34fd86-0462-42ac-90ae-4169c77e2f8e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
